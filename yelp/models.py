86198827b1e57e4e580cac77cf9bfe4eb7823176
Arleth Ontiveros <aontiveros@nyc.yearup.org>
2019-02-11 13:07:23 -0500

    classes

--- yelp/models.py
+++ yelp/models.py
@@ -5,3 +5,23 @@ class Food(models.Model):
     price = models.FloatField(default = 0)
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+
+class Customer(models.Model):
+    name = models.CharField(max_length = 200)
+    image = models.ImageField(default = 0)
+    phone = models.CharField(max_length = 200)
+    address = models.CharField(max_length = 200)
+
+class Order(models.Model):
+    food = models.CharField(max_length = 200)
+    quantity = models.CharFIeld(max_length = 200)
+    total = models.FloatField(default = 0)
+    customer = models.CharField(max_length =  200)
+    address = models.CharField(max_length = 200)
+
+class restaurant(models.Model):
+    name = models.CharFIeld(max_length = 200)
+    address = models.CharField(max_length = 200)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    phone = models.CharField(max_length = 200)
